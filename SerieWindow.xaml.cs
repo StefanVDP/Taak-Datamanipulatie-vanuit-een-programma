25ca392b9718f3bca3c9ffc42e2b9193cbad1453
StefanVDP <61277081+StefanVDP@users.noreply.github.com>
2020-05-04 19:52:06 +0200

    Add files via upload
    Het project waarmee we starten, dit heeft al de kleuren en een basis voor enkele windows

---
+++ SerieWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectDatamanipulatieAnime_WPF
+{
+    /// <summary>
+    /// Interaction logic for SerieWindow.xaml
+    /// </summary>
+    public partial class SerieWindow : Window
+    {
+        public SerieWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
